9e49c14e0e0b2f2f8626fcfafee6284552d16907
Andreas Sundqvist <andreas@stefna.is>
2021-07-12 15:33:36 +0200

    Remove parameter to avoid complaints

--- src/Tenant/KeyPrefixLoadTrait.php
+++ src/Tenant/KeyPrefixLoadTrait.php
@@ -8,11 +8,10 @@ use Circli\TenantExtension\Tenant;
 
 /**
  * @property KeyPrefixSecretsProvider $secretsManager
+ * @property string $searchKey
  */
 trait KeyPrefixLoadTrait
 {
-	private string $searchKey;
-
 	protected function loadTenantConfig(Tenant $tenant): void
 	{
 		$this->secretsManager->setPrefix('/' . $this->searchKey . '/tenant-' . $tenant->getId()->toString());
